cb0c280ad72dd5d5f81eeff39b3bb7cbea796478
FancyKillerPanda <fancykillerpanda@gmail.com>
2023-08-20 09:21:59 +1000

    Added dependencies SDL, SDL_image, glad, and glm.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
 #include "common.hpp"
+#include "utility/log.hpp"
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
 
 #include <stdio.h>
 
 s32 main(s32 argc, u8* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+        log::error("SDL_Init failed.\n%s", SDL_GetError());
+        return 1;
+    }
+
+    log::info("Initialised SDL.");
+
+    s32 imgInitFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+    if (IMG_Init(imgInitFlags) != imgInitFlags) {
+        log::error("IMG_Init() failed.\n%s", SDL_GetError());
+        return 1;
+    }
+
+    log::info("Initialised SDL_image.");
+
+    IMG_Quit();
+    log::info("Quit SDL_image.");
+
+    SDL_Quit();
+    log::info("Quit SDL.");
+
     return 0;
 }
